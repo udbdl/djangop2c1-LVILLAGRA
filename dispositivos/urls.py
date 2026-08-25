358f4c4f286509505ce1fa4eae1aa72cf204df3e
Laboratorio Computación N° 9 <claseslab9@inacap.cl>
2026-08-25 18:28:19 -0400

    creamos el template

--- dispositivos/urls.py
+++ dispositivos/urls.py
@@ -14,9 +14,19 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
-from django.urls import include, path
+from django.urls import path
+from . import views
+app_name = "dispositivos"
 urlpatterns = [
-    path("admin/", admin.site.urls),
-    path("", include("dispositivos.urls")),
+path("", views.inicio, name="inicio"),
+path(
+"zonas/<int:zona_id>/dispositivos/",
+views.dispositivos_zona,
+name="por_zona",
+),
+
+path("dispositivos/", views.catalogo, name="catalogo")
+
 ]
+
+
---
+++ dispositivos/views.py
@@ -0,0 +1,45 @@
+from django.shortcuts import render
+
+# Create your views here.
+from django.http import HttpResponse
+def inicio(request):
+    return HttpResponse(
+    "<h1>EcoEnergy</h1>"
+    "<p>Back End en funcionamiento</p>"
+    )
+
+def dispositivos_zona(request, zona_id):
+    if zona_id != 3:
+        return HttpResponse(
+            "Zona no encontrada", status=404
+)
+    return HttpResponse(
+        f"Dispositivos de la zona {zona_id}"
+    )
+
+from django.shortcuts import render
+
+def inicio(request):
+    contexto = {
+        "sistema": "EcoEnergy",
+        "mensaje": "Monitoreo energético responsable",
+        "asignatura": "Programación Back End",
+    }
+    return render(
+        request,
+        "dispositivos/inicio.html",
+        contexto,
+    )
+
+def catalogo(request):
+    dispositivos = [
+        {"nombre": "Medidor inteligente", "estado": "Activo"},
+        {"nombre": "Sensor de temperatura", "estado": "Activo"},
+        {"nombre": "Climatizador", "estado": "Revisión"},
+    ]
+    return render(
+        request,
+        "dispositivos/catalogo.html",
+        {"dispositivos": dispositivos},
+    )
+
